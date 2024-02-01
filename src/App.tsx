e0d1bdf8cdcd537888a6cea7079b6388c915efbc
vishnu9865 <sjeyavishnu9865@gmail.com>
2024-02-01 13:40:02 +0530

    chore: add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './styles/base.style.scss';
+import HomePage from "./pages/HomePage";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" Component={HomePage}/>
       </Routes>
     </BrowserRouter>
   )
